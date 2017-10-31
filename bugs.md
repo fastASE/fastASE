9e7ed7b8bf4aa145a146ea1385040f2753163874
ijayden-lung <ijayden.lung@gmail.com>
2017-10-31 18:32:07 +0800

    Fix Intron retention

---
+++ bugs.md
@@ -0,0 +1,5 @@
+###Mis-assigned MXE and SE
+grep MLPH ASEvent.tsv | less -SN
+
+###A3SS and A5SS >3 is not CSE
+grep MPRIP ASEvent.tsv | less -SN
--- test.pl
+++ test.pl
@@ -1,17 +1,47 @@
-my @a = qw/4 5 5/;
-my @b = qw/6 5 4 4 3/;
+#!/usr/bin/perl -w
 
-my $i = 0;
-foreach my $exon (@a){
-	if(grep {$_ eq $exon} @b){
-		$i++;
+my %retention;
+$retention{"exon5"}->{"105,120"} = 100;
+$retention{"exon5"}->{"102,190"} = 12;
+$retention{"exon5"}->{"178,199"} = 132;
+$retention{"exon5"}->{"200,300"} = 12;
+$retention{"exon5"}->{"230,300"} = 34;
+$retention{"exon5"}->{"240,400"} = 34;
+
+
+
+while(my ($exon,$val) = each %retention){
+	my %stat;
+	my $j = 0;
+	foreach my $key (sort keys %$val){
+		my ($str,$end) = split /,/,$key;
+		if($j == 0){
+			$stat{$str} = $end;
+		}
+		else{
+			my $i = 0;
+			while(my ($st,$en) = each %stat){
+				if($str<=$en && $end>=$st){
+					$i++;
+					if($end >$en){
+						$stat{$st} = $end;
+					}
+					if($str < $st){
+						$stat{$str} = $end;
+						delete $stat{$st};
+					}
+				}
+			}
+			if($i == 0){
+				$stat{$str} = $end;
+			}
+		}
+		$j++;
+	}
+	while(my ($key,$val) = each %stat){
+		print "$key\t$val\n";
 	}
 }
-print "$i\n";
 
-if(grep { ++$hash{$_} > 1 } @b){
-	print "it is A3SS\n";
-}
-else{
-	print "it is ALE\n";
-}
+
+
